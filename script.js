d77b6fd1b71b31dca493bff96e052def51bd102f
Ayaanle Nur <Ayaanle-n@hotmail.com>
2022-01-25 21:22:45 +0000

    added class of cities to html and put all the side list cities in one function rather than seperate functions

--- script.js
+++ script.js
@@ -10,18 +10,16 @@ const london = document.querySelector(".london");
 const amsterdam = document.querySelector(".amsterdam");
 const paris = document.querySelector(".paris");
 const rome = document.querySelector(".rome");
+const cities = document.querySelector('.cities')
 
 btn.addEventListener("click", fetchSearch);
 
 async function fetchSearch() {
   //used async function to fetch the data from the API
   const weatherResponse = await fetch(
-    "https://api.openweathermap.org/data/2.5/weather?q=" +
-      input.value +
-      "&appid=dbb45a93ce752788381a20675a5a9c02"
+    `https://api.openweathermap.org/data/2.5/weather?q=${input.value}&units=metric&appid=dbb45a93ce752788381a20675a5a9c02`
   );
   const data = await weatherResponse.json();
-  console.log(data);
 
   //displaying the classes on screen with innerHTML
   city.innerHTML = data.name;
@@ -33,93 +31,24 @@ async function fetchSearch() {
   let todaysDate = `${day}/${month}/${year}`;
   fullDate.innerHTML = todaysDate;
 
+  console.log(data.main);
   temperature.innerHTML = `${Math.floor(data.main.temp)}°`;
   humidity.innerHTML = `Humidity: ${data.main.humidity}%`;
   wind.innerHTML = `Wind: ${data.wind.speed} mph`;
 }
 
 //async functions for the cities on the side list
-london.addEventListener("click", getLondon);
+cities.addEventListener("click", getCities);
 
-async function getLondon() {
+async function getCities(e) {
   const weatherResponse = await fetch(
-    "https://api.openweathermap.org/data/2.5/weather?q=london&appid=dbb45a93ce752788381a20675a5a9c02"
+    `https://api.openweathermap.org/data/2.5/weather?q=${e.target.className}&units=metric&appid=dbb45a93ce752788381a20675a5a9c02`
   );
   const data = await weatherResponse.json();
-  console.log(data);
 
   city.innerHTML = data.name;
 
-  let date = new Date();
-  let day = date.getDate();
-  let month = date.getMonth() + 1;
-  let year = date.getFullYear();
-  let todaysDate = `${day}/${month}/${year}`;
-  fullDate.innerHTML = todaysDate;
-
-  temperature.innerHTML = `${Math.floor(data.main.temp)}°`;
-  humidity.innerHTML = `Humidity: ${data.main.humidity}%`;
-  wind.innerHTML = `Wind: ${data.wind.speed} mph`;
-}
-
-amsterdam.addEventListener("click", getAmsterdam);
-
-async function getAmsterdam() {
-  const weatherResponse = await fetch(
-    "https://api.openweathermap.org/data/2.5/weather?q=amsterdam&appid=dbb45a93ce752788381a20675a5a9c02"
-  );
-  const data = await weatherResponse.json();
-  console.log(data);
-
-  city.innerHTML = data.name;
-
-  let date = new Date();
-  let day = date.getDate();
-  let month = date.getMonth() + 1;
-  let year = date.getFullYear();
-  let todaysDate = `${day}/${month}/${year}`;
-  fullDate.innerHTML = todaysDate;
-
-  temperature.innerHTML = `${Math.floor(data.main.temp)}°`;
-  humidity.innerHTML = `Humidity: ${data.main.humidity}%`;
-  wind.innerHTML = `Wind: ${data.wind.speed} mph`;
-}
-
-paris.addEventListener("click", getParis);
-
-async function getParis() {
-  const weatherResponse = await fetch(
-    "https://api.openweathermap.org/data/2.5/weather?q=paris&appid=dbb45a93ce752788381a20675a5a9c02"
-  );
-  const data = await weatherResponse.json();
-  console.log(data);
-
-  city.innerHTML = data.name;
-
-  let date = new Date();
-  let day = date.getDate();
-  let month = date.getMonth() + 1;
-  let year = date.getFullYear();
-  let todaysDate = `${day}/${month}/${year}`;
-  fullDate.innerHTML = todaysDate;
-
-  temperature.innerHTML = `${Math.floor(data.main.temp)}°`;
-  humidity.innerHTML = `Humidity: ${data.main.humidity}%`;
-  wind.innerHTML = `Wind: ${data.wind.speed} mph`;
-}
-
-rome.addEventListener("click", getRome);
-
-async function getRome() {
-  const weatherResponse = await fetch(
-    "https://api.openweathermap.org/data/2.5/weather?q=rome&appid=dbb45a93ce752788381a20675a5a9c02"
-  );
-  const data = await weatherResponse.json();
-  console.log(data);
-
-  city.innerHTML = data.name;
-
-  let date = new Date();
+  let date = new Date()
   let day = date.getDate();
   let month = date.getMonth() + 1;
   let year = date.getFullYear();
